d0bbcbb3c31bc0f7ca178f132d4e1155d63e1479
Christian Nagel <christian@christiannagel.com>
2019-05-13 17:10:54 +0200

    tabs sample

---
+++ TabsSample/TabsSample/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TabsSample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private bool _isInitialized = false;
+        private void OnTabSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!_isInitialized)
+            {
+                _isInitialized = true;
+            }
+            else
+            {
+                MessageBox.Show("test");
+            }
+        }
+    }
+}
