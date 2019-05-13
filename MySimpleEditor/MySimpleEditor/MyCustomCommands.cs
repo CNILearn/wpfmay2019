bdb9f1b7a3bc9e0a4472696d887eed71350343ce
Christian Nagel <christian@christiannagel.com>
2019-05-13 15:16:31 +0200

    editor sample with commands and menu items

---
+++ MySimpleEditor/MySimpleEditor/MyCustomCommands.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MySimpleEditor
+{
+    public class MyCustomCommands
+    {
+        private static ICommand s_action1Command;
+        public static ICommand Action1Command
+        {
+            get => s_action1Command ?? (s_action1Command = new RoutedUICommand("Action1", "Action1", typeof(MyCustomCommands)));
+        }
+    }
+}
