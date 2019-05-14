8fd12f8232322c6153451c8fc161e75ba217270f
Christian Nagel <christian@christiannagel.com>
2019-05-14 14:41:10 +0200

    cursor change sample

---
+++ Tuesday/CursorChangeSample/CursorChangeSample/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CursorChangeSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public IEnumerable<string> CursorNames => Enum.GetNames(typeof(CursorType));
+        public string SelectedCursorName { get; set; }
+
+        private Cursor _prevCursor;
+        private void OnMouseEnter(object sender, MouseEventArgs e)
+        { 
+            Type cursorsType = typeof(Cursors);
+            var cursor = cursorsType.InvokeMember(SelectedCursorName, BindingFlags.GetProperty, null, null, null);
+            _prevCursor = this.Cursor;
+            if (cursor is Cursor newCursor)
+            {
+                this.Cursor = newCursor;
+            }
+        }
+
+        private void OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            this.Cursor = _prevCursor;
+            _prevCursor = null;
+        }
+    }
+}
