c0e6580de34344c8f122f7b499d7b4d5393c7367
Christian Nagel <christian@christiannagel.com>
2019-05-14 13:04:13 +0200

    data binding samples

---
+++ Tuesday/DelayBindingSample/DelayBindingSample/SomeData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelayBindingSample
+{
+    public class SomeData
+    {
+        private string _data1;
+
+        public string Data1
+        {
+            get { return _data1; }
+            set
+            {
+                _data1Values.Add(value);
+                _data1 = value;
+            }
+        }
+        private string _data2;
+
+        public string Data2
+        {
+            get { return _data2; }
+            set { _data2 = value; }
+        }
+
+        private ObservableCollection<string> _data1Values = new ObservableCollection<string>();
+        public IEnumerable<string> Data1Values => _data1Values;
+    }
+}
