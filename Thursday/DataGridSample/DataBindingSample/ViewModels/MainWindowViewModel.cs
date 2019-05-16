63e93c09a818dead4a189111d956d089e584ba45
Christian Nagel <christian@christiannagel.com>
2019-05-16 13:01:15 +0200

    data grid

---
+++ Thursday/DataGridSample/DataBindingSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DataBindingSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingSample.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public ObservableCollection<Book> Books { get; } = new ObservableCollection<Book>();
+
+        public ObservableCollection<string> Publishers { get; } = new ObservableCollection<string>();
+    }
+}
